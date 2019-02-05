3df7083a0dba9d247d4c6ff04341d8d48633f665
Dien-Nguyen-Sheridan <nguydien@sheridancollege.ca>
2019-02-05 10:22:30 -0500

    Add msg

--- Practice_GIt_2/JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ Practice_GIt_2/JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -15,7 +15,11 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello");
+        
     }
     
+    public static void hello(){
+        System.out.println("there");
+    }
 }
