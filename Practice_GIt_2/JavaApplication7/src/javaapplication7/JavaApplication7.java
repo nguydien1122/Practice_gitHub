8cbd7a2c06e65544a341948b6dd1a0b017d400ff
Dien-Nguyen-Sheridan <nguydien@sheridancollege.ca>
2019-02-05 10:20:02 -0500

    Practice git

---
+++ Practice_GIt_2/JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,21 @@
+/*
+ * Dien Nguyen
+ * Student ID: 991501600
+ * SYST10199 - Web Programming
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Dien
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
